1c794469c0cd04ebc7b23f8336198822f03ee4e8
jxmked <jovandeguia@gmail.com>
2025-12-23 10:36:16 +0800

    update

--- src/Enums.h
+++ src/Enums.h
@@ -5,19 +5,4 @@ enum class E_SignalLED { GESTURE, OVERRIDE, LOWBAT, BLUETOOTH };
 
 enum class MotorState { STOP, FORWARD, REVERSE, DISCONNECT };
 
-enum class ControllerState {
-  IDLE,
-  FORWARD,
-  REVERSE,
-  LEFT,
-  RIGHT,
-  STOP,
-  HARD_LEFT,
-  HARD_RIGHT,
-  R_LEFT,
-  R_RIGHT
-};
-
-enum class ClientState { DISCONNECTED, CONNECTING, CONNECTED };
-
 #endif
